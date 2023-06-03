d7e4bccff3eef8096a92a1bdcbad894f3fa0c8bd
sergiocardoso1 <sergioifbaiano@hotmail.com>
2023-06-02 22:53:01 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
  setInterval(function(){
    document.getElementById('slide' + contador).checked = true;
    contador++;
-   if(contador > 3){
+   if(contador > 7){
       contador = 1;
    }
  },4000)
